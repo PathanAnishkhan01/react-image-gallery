3816717dd4dc5cab95bac4556f4d16b3284e03da
ANISH <anishkhanypathan@gmailcom>
2025-12-25 15:52:12 +0530

    Save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -48,13 +48,13 @@ const App = () => {
 
   return (
     <div className="text-white bg-black w-full h-screen p-3 ">
-      <div className="flex flex-row flex-wrap justify-center overflow-scroll lg:overflow-hidden gap-1 w-full h-[calc(100%-12%)] ">
+      <div className="flex flex-row flex-wrap justify-around overflow-scroll lg:overflow-hidden gap-1.5  w-full lg:h-[calc(100%-12%)] h-[calc(100%-20%)] ">
         {loading &&
           Array.from({ length: minLength }).map((_, idx) => {
             return (
               <div
                 key={idx}
-                className="bg-gray-700 p-3 rounded-xl animate-pulse w-96 h-56 lg:w-56 lg:h-48"
+                className="bg-gray-700 p-3 rounded-xl animate-pulse w-80 h-56 lg:w-56 lg:h-48"
               >
                 <div className="bg-gray-100 w-full h-36  rounded-lg" />
                 <div className="h-4 bg-gray-200 mt-2 rounded w-32 mx-auto" />
@@ -71,7 +71,7 @@ const App = () => {
               }`}
             >
               <a href={elem.url} target="_blank">
-                <div className=" overflow-hidden bg-gray-300 rounded-2xl w-96 h-48 lg:w-56 lg:h-48">
+                <div className=" overflow-hidden bg-gray-300 rounded-2xl w-80 h-48 lg:w-56 lg:h-48">
                   <img
                     loading="lazy"
                     onLoad={handleImageLoad}
--- src/components/Botton.jsx
+++ src/components/Botton.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-const button = ({name ,disabled,onClick}) => {
+const button = ({name ,disabled =false,onClick}) => {
   return (
     <button
-          className= {`bg-blue-400 rounded-2xl px-4 py-1 font-bold uppercase active:scale-105 shadow-blue-50 shadow-sm inset-shadow-blue-50 inset-shadow-sm ${disabled ? "opacity-50 cursor-not-allowed": ""}`}
+          className= {`bg-blue-400 rounded-2xl px-4 py-1 font-bold uppercase shadow-blue-50 shadow-sm inset-shadow-blue-50 inset-shadow-sm ${disabled ? "opacity-50 cursor-not-allowed": "active:scale-105 "}`} 
           disabled={disabled}
-
           onClick={onClick}
         >
           {name}
